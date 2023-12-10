92ba5e3db8d74161f3fcbb30677c0afda1c3c332
CorMazz <95447242+CorMazz@users.noreply.github.com>
2023-12-10 15:12:24 -0500

    clear cache from github

--- cache/joblib/__main__-c%3A-users-mazzac3-documents-github-dancedjassistant-main/analyze_songs/func_code.py
+++
@@ -1,4 +0,0 @@
-# first line: 36
-    @memory.cache
-    def analyze_songs(song_id, song_name):
-        return dj.analyze_songs([song_id], [song_name], progress_bar=False)
--- cache/joblib/__main__-c%3A-users-mazzac3-documents-github-dancedjassistant-main/parse_playlist/func_code.py
+++
@@ -1,4 +0,0 @@
-# first line: 41
-    @memory.cache
-    def parse_playlist(playlist_id):
-        return dj.parse_playlist(playlist_id)
